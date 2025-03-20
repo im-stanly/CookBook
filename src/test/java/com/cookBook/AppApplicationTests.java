325ce786bf167c455645246a78092a9a7b456e57
im-stanly <stanislaw032002@o2.pl>
2025-03-20 20:41:49 +0100

    basic schema of Spring app

---
+++ src/test/java/com/cookBook/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cookBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
