282b9d960e1e35603a42a0eaf7d0880c440a124d
Rafal Przypek <joker.rafrom@gmail.com>
2025-03-30 21:11:56 +0200

    added: textfield to insert products

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -7,6 +7,9 @@ import { useEffect } from 'react';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { Header } from 'react-native/Libraries/NewAppScreen';
+import { useState } from 'react';
+import { TextInput, View, Text, Button, Pressable } from 'react-native';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -27,13 +30,68 @@ export default function RootLayout() {
     return null;
   }
 
+  const [page, setPage] = useState<'home' | 'recipe'>('home');
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
+        {page === 'home' ? (
+        <View 
+          style={{ 
+            flex: 1, 
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+            <Text>insert product</Text>
+            <TextInput 
+              style={{ 
+                height: 40, 
+                width: 200, 
+                borderColor: 'gray', 
+                borderWidth: 1, 
+                padding: 10,
+                margin: 10,
+                borderRadius: 10,
+              }}
+              placeholder="Product Name"
+            />
+            <Pressable 
+              style={{
+                margin: 10,
+                backgroundColor: 'green',
+                padding: 10,
+                borderRadius: 10,
+              }}
+              onPress={() => {
+                setPage('recipe');
+              }}
+            > 
+              <Text style={{ color: 'white' }}>get recipe</Text>
+            </Pressable>
+          </View>) : (
+            <View 
+            style={{ 
+              flex: 1, 
+              justifyContent: 'center',
+              alignItems: 'center',
+              }}>
+
+              <Text>recipe</Text>
+              
+              <Pressable 
+                style={{
+                  margin: 10,
+                  backgroundColor: 'green',
+                  padding: 10,
+                  borderRadius: 10,
+                }}
+                onPress={() => {
+                  setPage('home');
+                }}
+              > 
+                <Text style={{ color: 'white' }}>go back</Text>
+            </Pressable>
+          
+          </View>
+        )}
     </ThemeProvider>
   );
 }
