7c979db4d1e52ecbec365c60e3078e353ff7cf8c
Feq149 <konrad.ciborowski@student.uj.edu.pl>
2025-06-08 20:39:57 +0200

    added endpoints for reactions (temporary)

--- backend/src/main/java/com/cookBook/controller/RecipeController.java
+++ backend/src/main/java/com/cookBook/controller/RecipeController.java
@@ -3,8 +3,12 @@ package com.cookBook.controller;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import com.cookBook.dto.RecipeModelDTO;
 import com.cookBook.dto.UserInputIngredientDTO;
@@ -33,10 +37,10 @@ public class RecipeController {
     @Autowired
     private UserRepository userRepository;
 
-    @GetMapping("/all")
-    public List<RecipeModelDTO> getAllRecipes() {
-        return recipeService.getAllRecipes();
-    }
+//    @GetMapping("/all")
+//    public List<RecipeModelDTO> getAllRecipes() {
+//        return recipeService.getAllRecipes();
+//    }
 
     @PostMapping("/byIngredients")
     public Map<Integer, List<RecipeModelDTO>> getRecipe(@RequestHeader(value = "user-token") String userToken, @RequestBody List<UserInputIngredientDTO> ingredients){
@@ -44,7 +48,9 @@ public class RecipeController {
 //        if(!UserTokenUtils.isTokenValid(userToken))
 //            return null;
 //        if(!UserTokenUtils.isVerified(userToken))
-        return recipeService.getRecipesByIngredients(ingredients);
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+        return recipeService.getRecipesByIngredients(ingredients,username);
     }
 
     @PostMapping("/byText")
@@ -62,8 +68,9 @@ public class RecipeController {
         ingredientList.add(new UserInputIngredientDTO("Flour", "Cup", 10));
         ingredientList.add(new UserInputIngredientDTO("Cream", "Cup", 10));
         ingredientList.add(new UserInputIngredientDTO("Banana", "Piece", 10));
-
-        return recipeService.getRecipesByIngredients(ingredientList);
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+        return recipeService.getRecipesByIngredients(ingredientList,username);
     }
 
     @GetMapping("/ingredients/{ingName}")
@@ -77,22 +84,22 @@ public class RecipeController {
     public ResponseEntity<String> addReaction(
             @PathVariable long recipeId,
             @RequestParam boolean isLike,
-            @RequestParam long userId
+            @RequestParam String username
     ) {
         RecipeModel recipe = recipeRepository.findById(recipeId).orElse(null);
         if (recipe == null) {
             return ResponseEntity.badRequest().body("Recipe not found.");
         }
 
-        UserModel user = userRepository.findById(userId);
-        if (user == null) {
+        Optional<UserModel> userOPT = userRepository.findByUsername(username);
+        if (userOPT.isEmpty()) {
             return ResponseEntity.badRequest().body("User not found.");
         }
-
+        UserModel user = userOPT.get();
         ReactionModel reaction = reactionRepository
             .findAll()
             .stream()
-            .filter(r -> r.getUser().getId() == userId && r.getRecipe().getId() == recipeId)
+            .filter(r -> r.getUser().getUsername().equals(username) && r.getRecipe().getId() == recipeId)
             .findFirst()
             .orElse(new ReactionModel());
 
@@ -102,5 +109,13 @@ public class RecipeController {
 
         reactionRepository.save(reaction);
         return ResponseEntity.ok("Reaction recorded successfully.");
+    }
+    @DeleteMapping
+    private ResponseEntity<String> deleteReaction(
+            @PathVariable long recipeId,
+            @RequestParam String username
+    ) {
+        reactionRepository.deleteByUserUsernameAndRecipeId(username, recipeId);
+        return ResponseEntity.ok("Reaction deleted successfully");
     }
 }
--- backend/src/main/java/com/cookBook/repository/UserRepository.java
+++ backend/src/main/java/com/cookBook/repository/UserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import java.util.List;
+import java.util.Optional;
 
 import com.cookBook.entity.UserModel;
 
@@ -12,7 +13,7 @@ public interface UserRepository extends JpaRepository<UserModel, Long> {
 
 
     UserModel findById(long id);
-    List<UserModel> findByUsername(String username);
+    Optional<UserModel> findByUsername(String username);
     List<UserModel> findByEmail(String email);
     UserModel findByUsernameAndPassword(String username, String password);
 }
